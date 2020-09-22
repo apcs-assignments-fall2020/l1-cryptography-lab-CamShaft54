0f38a035d9625a6e9d64af0d5a12aa0ee4243d10
camshaft54 <cam.shaw1234@gmail.com>
2020-09-22 09:01:32 -0700

    Complete Caesar Methods

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,30 +1,26 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Caesar {
     public static String encryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        return Arrays.toString(message.chars().mapToObj(c -> (c >= 65 && c <= 90) ? (char) ((c - 65 + 3) % 26 + 65) : (c >= 97 && c <= 122) ? (char) ((c - 97 + 3) % 26 + 97) : (char) c).toArray()).replace("[", "").replace(", ","").replace("]", "");
     }
 
     public static String decryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        return Arrays.toString(message.chars().mapToObj(c -> (c >= 65 && c <= 90) ? (char) ((c - 65 + 26 - 3) % 26 + 65) : (c >= 97 && c <= 122) ? (char) ((c - 97 + 26 - 3) % 26 + 97) : (char) c).toArray()).replace("[", "").replace(", ","").replace("]", "");
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        return Arrays.toString(message.chars().mapToObj(c -> (c >= 65 && c <= 90) ? (char) ((c - 65 + key) % 26 + 65) : (c >= 97 && c <= 122) ? (char) ((c - 97 + key) % 26 + 97) : (char) c).toArray()).replace("[", "").replace(", ","").replace("]", "");
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        return Arrays.toString(message.chars().mapToObj(c -> (c >= 65 && c <= 90) ? (char) ((c - 65 + 26 - (key % 26)) % 26 + 65) : (c >= 97 && c <= 122) ? (char) ((c - 97 + 26 - key) % 26 + 97) : (char) c).toArray()).replace("[", "").replace(", ","").replace("]", "");
     }
 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         System.out.println("Would you like to \"encrypt\" or \"decrypt\" a message?");
         String command = scan.nextLine().trim().toLowerCase();
 
